ae740543da805dc7761111fff3597d3d0f6408bb
Rony Vidaur <rony.vidaur@gmail.com>
2017-04-10 18:04:49 -0600

    Data & methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: "Rony"
+    name: "Rony",
+    job: "Owl"
+  },
+  methods: {
+    greet: function(time) {
+      return `Good ${time} ${this.name}`
+    }
   }
 })
