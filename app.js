bb654907f47ebc005849668c0f48412c237b6d66
Rony Vidaur <rony.vidaur@gmail.com>
2017-04-14 11:28:01 -0600

    events

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ new Vue({
     name: "Rony",
     job: "Owl",
     website: "https://ronyvidaur.github.io",
-    websiteTag: '<a href="http://twitter.com">Twitter</a>'
+    websiteTag: '<a href="http://twitter.com">Twitter</a>',
+    age: 23
   },
   methods: {
     greet: function(time) {
       return `Good ${time} ${this.name}`
+    },
+    add: function(inc) {
+      this.age += inc
+    },
+    subtract: function(dec) {
+      this.age -= dec
     }
   }
 })
